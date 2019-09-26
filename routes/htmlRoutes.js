6c7b1388f3091480acbc9fa1102fb737779cda4a
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-09-26 13:49:31 -0700

    quick change

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,37 +2,33 @@ var path = require('path')
 
 module.exports = function (app) {
   // Load home page
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/Home.html"))
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
   })
 
   // Load hogwarts page with all houses on it
   app.get("/hogwarts", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/hogwarts.html"))
+    res.sendFile(path.join(__dirname, "../public/hogwarts.html"))
   })
 
   // Load gryffindor house page
   app.get("/gryffindor", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/gryffindor.html"))
+    res.sendFile(path.join(__dirname, "../public/gryffindor.html"))
   })
 
   // Load hufflepuff house page
   app.get("/hufflepuff", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/hufflepuff.html"))
+    res.sendFile(path.join(__dirname, "../public/hufflepuff.html"))
   })
 
   // Load ravenclaw house page
   app.get("/ravenclaw", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/ravenclaw.html"))
+    res.sendFile(path.join(__dirname, "../public/ravenclaw.html"))
   })
 
   // Load slytherin house page
   app.get("/slytherin", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/slytherin.html"))
+    res.sendFile(path.join(__dirname, "../public/slytherin.html"))
   })
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
 };
