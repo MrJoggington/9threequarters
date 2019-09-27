0692512b9f305bba64796977f3396266f95d42f0
Kyle McFadden <burntwist@sbcglobal.net>
2019-09-26 19:51:34 -0700

    add in some images for the house banners, start work on index page with the signup and sign in modals

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,30 +3,31 @@ var path = require("path");
 module.exports = function (app) {
   // Load Home page
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"))
+    res.sendFile(path.join(__dirname, "../public/html/home.html"))
   })
 
   //Load Hogwarts page with all houses on it
   app.get("/hogwarts", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/hogwarts.html"))
+    res.sendFile(path.join(__dirname, "../public/html/hogwarts.html"))
   })
 
   //Load Gryffindor house page
   app.get("/gryffindor", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/gryffindor.html"))
+    res.sendFile(path.join(__dirname, "../public/html/gryffindor.html"))
   })
 
   //Load Hufflepuff house page
   app.get("/hufflepuff", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/hufflepuff.html"))
+    res.sendFile(path.join(__dirname, "../public/html/hufflepuff.html"))
   })
 
   //Load Ravenclaw house page
   app.get("/ravenclaw", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/ravenclaw.html"))
+    res.sendFile(path.join(__dirname, "../public/html/ravenclaw.html"))
   })
 
   //Load Slytherin house page
   app.get("/slytherin", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/slytherin.html"))
+    res.sendFile(path.join(__dirname, "../public/html/slytherin.html"))
   })
+}
