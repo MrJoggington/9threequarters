2004b3ab11d5874b8052ed2629f01a43aff0ef40
Kyle McFadden <burntwist@sbcglobal.net>
2019-09-26 19:00:28 -0700

    add an html folder to public to clean up, add base headers to files and include boostrap cdn links

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,32 +3,32 @@ var path = require('path')
 module.exports = function (app) {
   // Load home page
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"))
+    res.sendFile(path.join(__dirname, "../public/html/home.html"))
   })
 
   // Load hogwarts page with all houses on it
   app.get("/hogwarts", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/hogwarts.html"))
+    res.sendFile(path.join(__dirname, "../public/html/hogwarts.html"))
   })
 
   // Load gryffindor house page
   app.get("/gryffindor", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/gryffindor.html"))
+    res.sendFile(path.join(__dirname, "../public/html/gryffindor.html"))
   })
 
   // Load hufflepuff house page
   app.get("/hufflepuff", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/hufflepuff.html"))
+    res.sendFile(path.join(__dirname, "../public/html/hufflepuff.html"))
   })
 
   // Load ravenclaw house page
   app.get("/ravenclaw", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/ravenclaw.html"))
+    res.sendFile(path.join(__dirname, "../public/html/ravenclaw.html"))
   })
 
   // Load slytherin house page
   app.get("/slytherin", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/slytherin.html"))
+    res.sendFile(path.join(__dirname, "../public/html/slytherin.html"))
   })
 
 };
