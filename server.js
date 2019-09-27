c40183473d034d260891fc59041fe8bf049fe8d3
Ghett Lee <leeghett@yahoo.com>
2019-09-26 19:05:29 -0700

    deleted handlebars

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+
 
 var db = require("./models");
-
+var passport = require("passport");
+var passportConfig = require("./config/passport")
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: "keyboard hero" }))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router)
+// static directory
 app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
@@ -34,8 +33,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
