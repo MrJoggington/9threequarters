f0165a548e52ddb57acf7b1693057f1ba3620db7
William Dunn <pikablu112@gmail.com>
2019-09-28 12:07:25 -0700

    Fixed API Routes???

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,39 +24,41 @@ module.exports = function (app) {
   });
   // Get all posts
   app.get("/api/platform", function (req, res) {
-    db.Platform.findAll({}).then(function (dbPlatform) {
-      res.json(dbPlatform);
+    db.findAll({}).then(function (results) {
+      res.json(results)
     });
   });
 
   // Create a new post
   app.post("/api/platform", function (req, res) {
-    db.Platform.create({
+    console.log("Thread Data:");
+    console.log(req.body);
+    db.create({
       title: req.body.title,
       body: req.body.body
-    }).then(function (dbPlatform) {
-      res.json(dbPlatform);
+    }).then(function (results) {
+      res.end();
     });
   });
 
   // Delete an post by id
   app.delete("/api/platform/:id", function (req, res) {
-    db.Platform.destroy({ where: { id: req.params.id } }).then(function (dbPlatform) {
-      res.json(dbPlatform);
+    db.destroy({ where: { id: req.params.id } }).then(function (results) {
+      res.json(results);
     });
   });
 
   //Update a post
   app.put("/api/platform", function (req, res) {
-    db.Platform.update({
+    db.update({
       title: req.body.title,
       body: req.body.body
     }, {
       where: {
         id: req.body.id
       }
-    }).then(function (dbPlatform) {
-      res.json(dbPlatform);
+    }).then(function (results) {
+      res.json(results);
     })
       .catch(function (err) {
         res.json(err);
