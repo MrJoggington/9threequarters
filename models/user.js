3738a34f31f12f8c39b6e65e341278ce53520c9f
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-10-04 12:36:49 -0700

    garbage

--- models/user.js
+++ models/user.js
@@ -1,22 +1,38 @@
-
-
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define('User', {
-        username: { type: DataTypes.STRING, unique: true, allowNull: false, validate: { notEmpty: true } },
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        house: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
         password: {
-            type: DataTypes.STRING, allowNull: false, validate: { notEmpty: true }
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
         }
-    },
-        {
-            dialect: 'mysql'
-        });
+    }, {
+        dialect: 'mysql'
+    });
 
-        User.associate = function(models) {
+    User.associate = function (models) {
 
-            User.hasMany(models.Post, {
-                onDelete: "cascade"
-            })
-        }
+        User.hasMany(models.Post, {
+            onDelete: "cascade"
+        })
+    }
 
     return User
 }
